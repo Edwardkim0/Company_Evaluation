af9f349e11c74e78c5db13978a869e29fc0fa8f7
Edwardkim0 <scminoov@gamil.com>
2020-02-15 11:01:04 +0900

    [Dev_Update] json indent 추가,
    기존 파일 indent = 4로 변경:

--- util/handling_json.py
+++ util/handling_json.py
@@ -11,12 +11,12 @@ def encoding_format_converter(file_name):
     with open(file_name, 'r', encoding='UTF-8') as f1:
         json_data = json.load(f1)
     with open(file_name, 'w', encoding='UTF-8') as json_writter:
-        json.dump(json_data, json_writter, ensure_ascii=False)
+        json.dump(json_data, json_writter, indent=4,ensure_ascii=False)
     pprint(json_data)
 
 
 if __name__ == '__main__':
-    directory = "C:\\project\\code\\ai_company_evaluatuion\\ai portfolio\\crawling_data\\edit"
+    directory = "C:\\project\\code\\ai_company_evaluatuion\\ai portfolio\\crawling_data"
     for file_name in os.walk(directory).__next__()[2]:
         file_path = os.path.join(directory,file_name)
         encoding_format_converter(file_path)
